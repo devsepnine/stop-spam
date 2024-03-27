bd7ff3232adcb1e107a2d898c665a92402edfbba
codenyang <devsepnine@hotmail.com>
2024-03-27 21:26:43 +0900

    fix: config 파일 수정

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ```
 copy config/config.example.json to config/config.json
 ```
+```json
+{
+  "discord_token": "discord_token", // discord bot token
+  "summon_timeout": 10, // 연속 입력 시간
+  "white_list": null, // 허용된 유저 ID
+  "limit_count": 3 // 최대 연속 입력 횟수
+}
+```
--- util/util.go
+++ util/util.go
@@ -8,11 +8,15 @@ import (
 )
 
 type Config struct {
-	Guild         string   `json:"guild"`
 	DiscordToken  string   `json:"discord_token"`
 	SummonTimeout int      `json:"summon_timeout"`
 	WhiteList     []string `json:"white_list"`
+	LimitCount    int      `json:"limit_count"`
 }
+
+const (
+	filePath = "./config/config.json"
+)
 
 var config Config
 
@@ -20,7 +24,7 @@ var once sync.Once
 
 func GetConfig() Config {
 	once.Do(func() {
-		configFile, err := os.Open("./config/config.json")
+		configFile, err := os.Open(filePath)
 		defer func(configFile *os.File) {
 			err := configFile.Close()
 			if err != nil {
@@ -37,7 +41,6 @@ func GetConfig() Config {
 }
 
 func UpdateConfig(key string, value interface{}) error {
-	fmt.Println(key, value)
 	fmt.Printf("type %v \n", value)
 
 	switch value.(type) {
@@ -45,11 +48,11 @@ func UpdateConfig(key string, value interface{}) error {
 	//	//tv := value.(string)
 	//	//config.WhiteList = tv
 	case int:
-		tv := int(value.(int))
+		tv := value.(int)
 		config.SummonTimeout = tv
 	}
 
-	configFile, err := os.ReadFile("./config/config.json")
+	configFile, err := os.ReadFile(filePath)
 	if err != nil {
 		return fmt.Errorf("파일을 읽을 수 없습니다. : %v", err)
 	}
@@ -66,7 +69,7 @@ func UpdateConfig(key string, value interface{}) error {
 		return fmt.Errorf("fail Marshal %v", err)
 	}
 
-	err = os.WriteFile("./config/config.json", updatedData, 0644)
+	err = os.WriteFile(filePath, updatedData, 0644)
 	if err != nil {
 		return fmt.Errorf("파일 업데이트에 실패 하였습니다. %v", err)
 	}
